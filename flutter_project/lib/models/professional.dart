d5f0a5686ad94b1f2eb8ec0dc57f1dc6eb2d4a31
IA990 <xsrsia990sensort@gmail.com>
2025-07-24 00:04:13 +0100

    Add flutter_project/lib/models/professional.dart 1753311843158

---
+++ flutter_project/lib/models/professional.dart
@@ -0,0 +1,16 @@
+// Model class for Professional in MaBoutique.ma app
+class Professional {
+  final String name;
+  final String type;
+  final String phone;
+  final String location;
+  final String photoUrl;
+
+  Professional({
+    required this.name,
+    required this.type,
+    required this.phone,
+    required this.location,
+    required this.photoUrl,
+  });
+}
